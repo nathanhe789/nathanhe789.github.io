723cceac26d0cf45191a9e442e2fd72a5c455dc0
CSSI 2015 <CSSI@CSSIs-MacBook-Air.local>
2015-07-21 22:54:57 -0500

    second commit, revised webpage

---
+++ my_script.js
@@ -0,0 +1,20 @@
+//  N.I.C.O
+// Name, Input, Code, Output
+function goToLunch(student){
+  // Instruction go here.
+  console.log(student + ", walk around desk");
+  console.log(student + ", turn right");
+  console.log(student + ", walk 10 paces");
+  console.log(student + ", grab door handle");
+  console.log(student + ", turn handle");
+  console.log(student + ", walk out door");
+  return student.toUpperCase();
+}
+
+function add(firstNumber, secondNumber){
+  return firstNumber + secondNumber;
+}
+
+goToLunch("Shaun"); //var student = "Shaun";
+goToLunch("Elisabeth"); //var student = "Elisabeth";
+goToLunch("Jackie");  //var student = "Jackie"
